c2d0f00f7d5115ad10211128ee03a243f1b1c387
DangQuangThang170605 <thangdq1083@ut.edu.vn>
2025-11-04 09:45:25 +0700

    Create JwtTokenProvider.java

---
+++ backend/ev_co_ownership/src/java/com/ev_co_ownership/security/JwtTokenProvider.java
@@ -0,0 +1,39 @@
+package com.ev_co_ownership.security;
+
+import io.jsonwebtoken.*;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    private final String SECRET_KEY = "evco_secret_key";
+    private final long EXPIRATION_TIME = 86400000; // 1 ngày
+
+    public String generateToken(String email, String role) {
+        return Jwts.builder()
+                .setSubject(email)
+                .claim("role", role)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+                .compact();
+    }
+
+    public String getEmailFromToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+}
