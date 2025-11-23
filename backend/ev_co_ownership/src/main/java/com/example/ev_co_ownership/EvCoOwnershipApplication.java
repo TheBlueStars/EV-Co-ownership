b6187323be8f81ea3efdea3247c2341607b268a9
Dang Quang Thang <dangquangthangk9@gmail.com>
2025-11-23 15:32:44 +0700

    Update microservices + Docker

--- backend/ev_co_ownership/src/main/java/com/example/ev_co_ownership/EvCoOwnershipApplication.java
+++ backend/ev_co_ownership/src/main/java/com/example/ev_co_ownership/EvCoOwnershipApplication.java
@@ -1,4 +1,8 @@
+<<<<<<< Updated upstream:backend/ev_co_ownership/src/main/java/com/example/ev_co_ownership/EvCoOwnershipApplication.java
 package com.example.ev_co_ownership;
+=======
+package com.project.evco;
+>>>>>>> Stashed changes:backend/src/main/java/com/project/evco/EvcoBackendApplication.java
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
